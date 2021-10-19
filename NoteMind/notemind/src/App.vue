0ff819b1c89734534e762c3b3185392662442f43
cyywhu <49968421+cyywhu@users.noreply.github.com>
2021-10-19 14:05:07 +0800

    notemind大致框架

---
+++ NoteMind/notemind/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <workspace></workspace>
+  </div>
+</template>
+
+<script>
+import workspace from '@/views/workspace.vue'
+
+export default {
+  components: {
+    workspace
+  }
+}
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ NoteMind/notemind/src/components/navigation/search.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="searchContent">
+    <div class="searchBar">
+      <img src="@/assets/pictures/navigation/search.png" class="searchPic">
+      <input placeholder="全局搜索" class="searchInput">
+    </div>
+    <div class="addButton">
+      <img src="@/assets/pictures/navigation/plus.png" class="addIcon">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
+<style>
+  .searchContent{
+    display: flex;
+  }
+
+  .searchBar{
+    display: flex;
+    align-items: center;
+    height: 35px;
+    width: 212px;
+    border-radius: 7px;
+    background-color: #E5E5E5;
+  }
+
+  .searchPic{
+    height: 15px;
+    margin-left: 10.38px;
+  }
+
+  .searchInput{
+    outline: none;
+    border: 0;
+    padding-left: 7.78px;
+    background-color: #E5E5E5;
+  }
+
+  input::-webkit-input-placeholder{
+    color: #B3B3B3;
+    font-size: 13px;
+  }
+
+  .addButton{
+    display: flex;
+    height: 35px;
+    width: 35px;
+    margin-left: 11px;
+    border-radius: 35px;
+    background-color: black;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .addIcon{
+    width: 12.25px;
+    height: 12.25px;
+  }
+</style>
