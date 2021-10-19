0b199911b852fdb5f3d5c7d65bcfa27c501019fe
cyywhu <49968421+cyywhu@users.noreply.github.com>
2021-10-19 14:40:36 +0800

    notemind框架

---
+++ src/components/navigation/navigaitonItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="selectedNav">
+    <div class="navIcon"><slot name="navIcon"></slot></div>
+    <div class="navFont menuName">{{menuName}}</div>
+    <img src="@/assets/pictures/navigation/plus-black.png" name="plusIcon" class="plusIcon">
+    <img src="@/assets/pictures/navigation/ttriangle.png" class="triangleIcon">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navigation-item',
+  props: ['menuName']
+}
+</script>
+
+<style>
+  .selectedNav{
+    display: flex;
+    height: 35px;
+    width: 258px;
+    margin-left: 17px;
+    background-image: url(../../assets/pictures/navigation/navSelected.png);
+    align-items: center;
+  }
+
+  .navIcon{
+    width: 15px;
+    margin-left: 7px;
+  }
+
+  .menuName{
+    margin-left: 10.83px;
+    flex-grow: 1;
+  }
+
+  .plusIcon{
+    width: 9.92px;
+    height: 9.92px;
+    margin-right: 9.54px;
+  }
+
+  .triangleIcon{
+    width: 12px;
+    margin-right: 11px;
+  }
+</style>
---
+++ src/components/navigation/user.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="userNav">
+    <img src="@/assets/pictures/navigation/user1.jpg" class="userImg">
+    <div class="navFont userName">杨天悦</div>
+    <img src="@/assets/pictures/navigation/rtriangle.png" class="triangle">
+    <img src="@/assets/pictures/navigation/userMessage.png" class="messageIcon">
+  </div>
+</template>
+
+<script>
+import '@/assets/css/font.css'
+
+export default {
+  name: 'user'
+}
+</script>
+
+<style>
+  .userNav{
+    display: flex;
+    vertical-align: middle;
+    align-items: center;
+  }
+
+  .userImg{
+    width: 50px;
+    border-radius: 50px;
+  }
+
+  .userName{
+    line-height: 50px;
+    margin-left: 15px;
+  }
+
+  .triangle{
+    height: 13px;
+    width: 13px;
+    margin-left: 10px;
+  }
+
+  .messageIcon{
+    width: 14px;
+    margin-left: 105px;
+  }
+</style>
